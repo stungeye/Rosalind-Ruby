77d10042e518bbbafae14bbfb631887195a18fc3
Wally Glutton <stungeye@gmail.com>
2012-12-12 12:27:32 -0600

    DNA class and specs are complete. All tests pass. Renamed files.

--- Counting DNA Nucleotides/dna_spec.rb
+++ Counting DNA Nucleotides/dna_spec.rb
@@ -5,6 +5,7 @@ describe DNA do
   before do
     @empty_dna = DNA.new
     @short_dna = DNA.new('ACGT')
+    @sample_rosalind_dna = DNA.new('AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC')
   end
   
   describe "when asked for its class" do
@@ -26,7 +27,7 @@ describe DNA do
     end
   end
   
-  describe "with initialized with 'ACGT'" do
+  describe "when initialized with 'ACGT'" do
     it "must respond with a length of four" do
       @short_dna.length.must_equal 4
     end
@@ -35,6 +36,21 @@ describe DNA do
       @short_dna.count_C.must_equal 1
       @short_dna.count_G.must_equal 1
       @short_dna.count_T.must_equal 1
+    end
+  end
+  
+  describe "when initialized with Rosalind sample dataset" do
+    it "must respond with a length of seventy" do
+      @sample_rosalind_dna.length.must_equal 70
+    end
+    it "must respond with the correct Rosalind sample output" do
+      @sample_rosalind_dna.count_A.must_equal 20
+      @sample_rosalind_dna.count_C.must_equal 12
+      @sample_rosalind_dna.count_G.must_equal 17
+      @sample_rosalind_dna.count_T.must_equal 21
+    end
+    it "must generate the correct ASCII count output" do
+      @sample_rosalind_dna.ASCII_ACGT_counts.must_equal "20 12 17 21"
     end
   end
 end

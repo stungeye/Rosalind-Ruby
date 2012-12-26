ea0a7a8b2567dc6be75dd18e516085c29b47b44e
Wally Glutton <stungeye@gmail.com>
2012-12-26 12:59:46 -0600

    Modified main.rb such that one command line argument is assumed.

--- challenges/Enumerating Gene Orders/main.rb
+++ challenges/Enumerating Gene Orders/main.rb
@@ -1,18 +1,16 @@
+# 
+
 unless ARGV.length.zero?
   
-  ARGV.each do |a|
-    
-    permutation_length = a.to_i
-    
-    integer_set = []
-    permutation_length.times { |i| integer_set << (i + 1) }
-    
-    all_permuations = integer_set.permutation.to_a
-    
-    puts all_permuations.length
-    all_permuations.each { |set| puts set.join(' ') }
-    
-  end
+  permutation_length = ARGV[0].to_i
+  
+  integer_set = []
+  permutation_length.times { |i| integer_set << (i + 1) }
+  
+  all_permuations = integer_set.permutation.to_a
+  
+  puts all_permuations.length
+  all_permuations.each { |set| puts set.join(' ') }
   
 end
 
